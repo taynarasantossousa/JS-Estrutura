7f435b7eb7cc563160ecafc771a7dce5a28f2dab
Taynara <taynarasantossousa09@gmail.com>
2025-06-09 19:43:39 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var res = document.querySelector(".res")
-var numero = Number(prompt("Digite umnumero para descobrir a tabuada"))
+var numero = Number(prompt("Digite um numero para descobrir a tabuada"))
 
 for(var inicio = 1; inicio  <= 10 ;inicio ++){
     res.innerHTML += `<p>${numero} x ${inicio} = 
